0b145ee778a862072d4b1e3b532d6270b50d694c
Julie Wagner <julie.wagner@est.fujitsu.com>
2016-09-23 11:32:50 +0200

    mongoDB access

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+
+
+var MongoClient = require('mongodb').MongoClient;
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,6 +13,29 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var mongo = process.env.VCAP_SERVICES;
+var port = process.env.PORT || 3001;
+var conn_str = "";
+
+if (mongo) {
+  console.log('i am in the cloud');
+  console.log(mongo);
+  var env = JSON.parse(mongo);
+  if (env['mongodb']) {
+    mongo = env['mongodb'][0]['credentials'];
+    if (mongo.url) {
+      conn_str = mongo.url;
+    } else {
+      console.log("No mongo found");
+    }
+  } else {
+    conn_str = 'mongodb://localhost:27017';
+  }
+} else {
+  console.log('I am running locally');
+  conn_str = 'mongodb://localhost:27017';
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,8 +52,76 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.get('/world', function(req, res) {
-  res.json({msg: "Hello World"})
-})
+
+
+  MongoClient.connect(conn_str, function(err, db) {
+
+    var message = {"msg": "test2"};
+    if (db && db !== "null" && db !== "undefined") {
+
+      db.collection('messages').insert(message, {safe:true}, function(err){
+        if (err) {
+          console.log(err.stack);
+          res.write('mongodb message insert failed');
+          res.end();
+        } else {
+          res.write('following messages has been inserted into database' + "\n"
+              + JSON.stringify(message));
+          res.end();
+        }
+      });
+    } else {
+      res.write('No mongo found');
+      res.end();
+    }
+
+    if(err){
+      console.log(err);
+    }
+
+  });
+
+ // res.json({msg: "Hello World. I am writing to database - "});
+});
+
+
+
+app.get('/read', function(req, res) {
+
+  var display = '';
+  MongoClient.connect(conn_str, function(err, db) {
+
+
+    if (db && db !== "null" && db !== "undefined") {
+
+      var myDocs = db.collection('messages').find();
+
+
+      myDocs.forEach(function(e){
+        console.log(e);
+        display = display.concat(e.msg + "\n");
+        console.log(display);
+
+      });
+
+
+      // console.log('read' + display);
+      // res.write('read out ' + display);
+      // res.end();
+    } else {
+      res.write('No mongo found');
+      res.end();
+    }
+
+    if(err){
+      console.log(err);
+    }
+
+  });
+  res.write('read out ' + display);
+  res.end();
+  // res.json({msg: "Hello World. I am writing to database - "});
+});
 
 
 
@@ -64,4 +159,4 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(3000);
+app.listen(port);
